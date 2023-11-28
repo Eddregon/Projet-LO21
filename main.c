d9c5b48e93efc134cd6abb7e15831a6cb7b92840
SpeedTomy <thoma3304@gmail.com>
2023-11-28 17:09:47 +0100

    correction

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int main(){
 
     // On crée une base de connaissance vide
-    base_connaissances = creer_base_vide();
+    liste_BC* base_connaissances = creer_base_vide();
     // On crée une base de faits vide
-    base_faits = creer_base_vide_BF();
+    liste_BF* base_faits = creer_base_vide_BF();
 
     // on charge les valeurs de la base de connaissances et de la base de faits et on les introduits dans les bases vides à l'aide des fonctions nécessaires
     charger_base_de_connaissances_et_faits("base_de_connaissances.txt", base_connaissances, base_faits);
@@ -15,7 +15,7 @@ int main(){
         // on crée un pointeur sur la première proposition de la base de faits
         proposition * fait = base_faits->BF;
         // on crée un pointeur sur la première regle de la base de connaissances
-        regle * regle = acceder_regle_tete(base_connaissances);
+        elem_BC * regle = acceder_regle_tete(base_connaissances);
 
         // tant qu'il y a un fait dans la base de faits
         while (fait!=NULL){
@@ -32,7 +32,7 @@ int main(){
                     }
                 }
                 // on passe à la regle suivante
-                regle = regle->next;
+                regle = regle->prochain;
             }
 
             // On regarde si il y a un prochain fait dans la base de faits sinon on retourne null et on arrete le moteur d'inference
@@ -45,6 +45,45 @@ int main(){
         printf("%s\n", liste_fait->value);
         liste_fait = liste_fait->next;
     }
+
+// Libération de la base de faits
+proposition *facteur_actuel, *prochain_facteur;
+facteur_actuel = base_faits->BF;
+while (facteur_actuel != NULL) {
+    prochain_facteur = facteur_actuel->next;
+    free(facteur_actuel->value); // Si 'value' est alloué dynamiquement
+    free(facteur_actuel);
+    facteur_actuel = prochain_facteur;
+}
+free(base_faits);
+
+// Libération de la base de connaissances
+elem_BC *regle_actuelle, *prochaine_regle;
+regle_actuelle = base_connaissances->BC;
+while (regle_actuelle != NULL) {
+    prochaine_regle = regle_actuelle->prochain;
+
+    // Libération des propositions dans la prémisse
+    proposition *prop_actuelle, *prochaine_prop;
+    prop_actuelle = regle_actuelle->premisse;
+    while (prop_actuelle != NULL && prop_actuelle != regle_actuelle->conclusion) {
+        prochaine_prop = prop_actuelle->next;
+        free(prop_actuelle->value);
+        free(prop_actuelle);
+        prop_actuelle = prochaine_prop;
+    }
+
+    // Libération de la conclusion si elle est différente de NULL
+    if (regle_actuelle->conclusion != NULL) {
+        free(regle_actuelle->conclusion->value);
+        free(regle_actuelle->conclusion);
+    }
+
+    free(regle_actuelle);
+    regle_actuelle = prochaine_regle;
+}
+free(base_connaissances);
+    return 0;
 }
 
 // moteur d'inférence
